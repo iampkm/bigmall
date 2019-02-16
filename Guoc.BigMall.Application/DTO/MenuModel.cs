42d18a8f14a7e6c8f7468174011baab01fbedf83
iampkm <chunguoc@163.com>
2019-02-16 12:18:29 +0800

    系统迁移mysql

--- Guoc.BigMall.Application/DTO/MenuModel.cs
+++ Guoc.BigMall.Application/DTO/MenuModel.cs
@@ -1,6 +1,4 @@
-﻿using FluentValidation;
-using FluentValidation.Results;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
